9faa5aa815fe68a411e82cbd404fa9a0a7c9c225
BrendonGS <bgsggermany@gmail.com>
2019-07-27 13:55:01 -0500

    original commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api do
+    get '/string_params_example' => 'params_examples#string_example'
+    get '/url_segment_example/:wildcard' => 'params_examples#url_example'
+    post 'body_params_example' => 'params_examples#body_params_example'
+    # get '/category/:category_type/product/:product_name'
+
+  end
+end
